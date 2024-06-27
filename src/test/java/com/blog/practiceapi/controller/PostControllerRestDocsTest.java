f9591ce2927e46b5c21fe88c1c64420f950667e1
tahyo27 <tahyo27@gmail.com>
2024-06-27 15:44:02 +0900

    rest docs 추가

--- src/test/java/com/blog/practiceapi/controller/PostControllerRestDocsTest.java
+++ src/test/java/com/blog/practiceapi/controller/PostControllerRestDocsTest.java
@@ -4,6 +4,8 @@ package com.blog.practiceapi.controller;
 import com.blog.practiceapi.domain.Post;
 import com.blog.practiceapi.repository.PostRepository;
 import com.blog.practiceapi.request.CreatePost;
+import com.blog.practiceapi.request.EditPost;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -28,7 +30,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @AutoConfigureRestDocs(uriScheme = "https", uriHost = "api.myblog.com", uriPort = 443)
 @ExtendWith(RestDocumentationExtension.class)
-public class PostControllerDocTest {
+public class PostControllerRestDocsTest {
 
     @Autowired
     private PostRepository postRepository;
@@ -36,7 +38,7 @@ public class PostControllerDocTest {
     private MockMvc mockMvc;
 
     @Test
-    @DisplayName("Rest Docs 1개 조회")
+    @DisplayName("Rest Docs 포스트 조회")
     void rest_docs_get_post_test() throws Exception {
         //given
         Post post = Post.builder()
@@ -46,12 +48,13 @@ public class PostControllerDocTest {
 
         postRepository.save(post);
 
+
         //expected
-        this.mockMvc.perform(RestDocumentationRequestBuilders.get("/posts/{postId}", 1L)
+        this.mockMvc.perform(RestDocumentationRequestBuilders.get("/posts/{postId}", post.getId())
                         .accept(MediaType.APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andDo(document("index" , pathParameters(
+                .andDo(document("post-inquiry" , pathParameters(
                         parameterWithName("postId").description("포스트 ID")
                         ),
                         responseFields (
@@ -63,7 +66,7 @@ public class PostControllerDocTest {
     }
 
     @Test
-    @DisplayName("Rest Docs 1개 등록")
+    @DisplayName("Rest Docs 포스트 등록")
     void rest_docs_write_post_test() throws Exception {
         //given
         CreatePost createPost = CreatePost.builder()
@@ -80,9 +83,68 @@ public class PostControllerDocTest {
                 )
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andDo(document("index" , requestFields(
+                .andDo(document("post-regist" , requestFields(
                                 fieldWithPath("title").description("포스트 제목"),
                                 fieldWithPath("content").description("포스트 내용"))
+                                )
+                );
+    }
+
+    @Test
+    @DisplayName("Rest Docs 포스트 수정")
+    void rest_docs_update_post_test() throws Exception {
+        //given
+        Post post = Post.builder()
+                .title("제목")
+                .content("내용")
+                .build();
+        postRepository.save(post);
+        long postId = post.getId();
+
+        EditPost editPost = EditPost.builder()
+                .title("업데이트한 제목")
+                .content("업데이트한 내용")
+                .build();
+
+        String json = (new ObjectMapper()).writeValueAsString(editPost);
+
+        //expected
+        this.mockMvc.perform(RestDocumentationRequestBuilders.patch("/posts/{postsId}", postId)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(json)
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andDo(document("post-update", pathParameters(
+                                parameterWithName("postsId").description("포스트 ID")
+                                ),requestFields(
+                                fieldWithPath("title").description("포스트 제목"),
+                                fieldWithPath("content").description("포스트 내용"))
+                                )
+                );
+
+    }
+
+    @Test
+    @DisplayName("Rest Docs 포스트 삭제")
+    void rest_docs_post_delete_test() throws Exception {
+        //given
+        Post post = Post.builder()
+                .title("제목")
+                .content("내용")
+                .build();
+        postRepository.save(post);
+        long postId = post.getId();
+
+
+        //expected
+        this.mockMvc.perform(RestDocumentationRequestBuilders.delete("/posts/{postsId}", postId)
+                        .accept(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andDo(document("post-delete", pathParameters(
+                                        parameterWithName("postsId").description("포스트 ID"))
                                 )
                 );
     }
