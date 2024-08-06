e73794c071579e2c050f2a8011807158f8edf712
tahyo27 <tahyo27@gmail.com>
2024-08-06 21:38:01 +0900

    이미지 임시

---
+++ src/main/java/com/blog/practiceapi/common/GoogleStorageUtil.java
@@ -0,0 +1,95 @@
+package com.blog.practiceapi.common;
+
+import com.google.cloud.storage.Blob;
+import com.google.cloud.storage.BlobId;
+import com.google.cloud.storage.BlobInfo;
+import com.google.cloud.storage.Storage;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class GoogleStorageUtil {
+
+    @Value("${spring.cloud.gcp.storage.bucket}")
+    private String bucketName;
+
+    private static final String GCS_URI_PREFIX = "https://storage.googleapis.com/";
+
+    private final Storage storage;
+
+    public boolean imgUpload(ImageNameParser imageNameParser) {
+        if(imageNameParser == null) {
+            log.info("테스트");
+        }
+        //임시 저장 이미지 경로
+        Path tempFilePath = Paths.get("./temp/image").resolve(imageNameParser.getTempName());
+        log.info(">>>>>>>>>>>>>> 이미지 들어옴 : {}", tempFilePath);
+        try {
+            byte[] imageFile = Files.readAllBytes(tempFilePath);
+
+            String contentType = getContentType(imageNameParser);
+
+            BlobInfo blobInfo = BlobInfo.newBuilder(bucketName, imageNameParser.getUuidName())
+                    .setContentType(contentType)
+                    .build();
+
+            Blob blob = storage.create(blobInfo, imageFile);
+
+            return blob != null && blob.getBlobId() != null;
+
+        } catch (IOException e) {
+            log.info("테스트");
+        }
+        return false; //임시
+    }
+
+    public boolean imgDelete(String path) {
+        if(path == null) {
+            return false;
+        }
+        String imgName = path.replace(GCS_URI_PREFIX + bucketName + "/", ""); //todo 유니크이름 도 가져와야할듯
+        Blob blob = storage.get(bucketName, imgName);
+        if (blob == null) {
+            log.info(">>>>>>>>> 해당 이미지 파일 스토리지에 없음 {}", imgName);
+            return false;
+        }
+        BlobId idWithGeneration = blob.getBlobId();
+        log.info(">>>>>>>>>>>>>>>>>>>>>> idWithGeneration {}", idWithGeneration);
+
+        boolean deleted = storage.delete(idWithGeneration);
+
+        if (deleted) {
+            log.info(bucketName + "의 Object " + path + "가 삭제되었습니다 ");
+        } else {
+            log.warn(bucketName + "에서"  + path + "의 삭제가 실패했습니다" );
+        }
+
+        return deleted;
+    }
+
+    private static String getContentType(ImageNameParser imageNameParser) {
+        String extension = imageNameParser.getExtension(); //확장자
+
+        String contentType = MediaType.APPLICATION_OCTET_STREAM_VALUE;
+
+        if (extension.endsWith(".png")) {
+            contentType = MediaType.IMAGE_PNG_VALUE;
+        } else if (extension.endsWith(".jpg") || extension.endsWith(".jpeg")) {
+            contentType = MediaType.IMAGE_JPEG_VALUE;
+        } else if (extension.endsWith(".gif")) {
+            contentType = MediaType.IMAGE_GIF_VALUE;
+        }
+        return contentType;
+    }
+
+}
---
+++ src/main/java/com/blog/practiceapi/common/ImageNameParser.java
@@ -0,0 +1,37 @@
+package com.blog.practiceapi.common;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Getter
+@Setter
+@ToString
+public class ImageNameParser {
+
+    private final String originName;
+    private final String uuidName;
+    private final String gcsPath;
+    private final String tempName;
+    private final String extension;
+    public ImageNameParser(String src) { //todo 주소처리 고민
+
+        String replacedName = src.replace("/temp/image/", "");
+        String[] parts = replacedName.split("_", 2);
+        String origin = parts[1];
+        String fileExtension = origin != null && origin.contains(".") //확장자 추출
+                ? origin.substring(origin.lastIndexOf('.')) : "";
+        String unique = parts[0] + fileExtension;
+        String path = "https://storage.googleapis.com/imgtest_bucket/" + unique;
+
+        this.extension = fileExtension;
+        this.tempName = replacedName;
+        this.originName = origin;
+        this.uuidName = unique;
+        this.gcsPath = path;
+
+    }
+
+}
